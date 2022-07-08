08a0a64487bca301d7dd0082714166aeb6c72a41
c-mi <cegielski.michal.work@gmail.com>
2022-07-08 09:38:44 +0200

    Add simple eks directory

---
+++ k8s/eks/module_eks.tf
@@ -0,0 +1,63 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = ">= 18.26"
+
+  cluster_name    = local.name
+  cluster_version = "1.22"
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns = {
+      resolve_conflicts = "OVERWRITE"
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      addon_version     = "v1.11.2-eksbuild.1"
+      resolve_conflicts = "OVERWRITE"
+    }
+  }
+
+  cluster_encryption_config = [{
+    provider_key_arn = aws_kms_alias.this.target_key_arn
+    resources        = ["secrets"]
+  }]
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    remote_access = {
+      ec2_ssh_key               = aws_key_pair.this.id
+      source_security_group_ids = [aws_security_group.ssh_sg.id]
+    }
+    iam_role_additional_policies = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+    create_launch_template       = false
+    launch_template_name         = ""
+  }
+
+  eks_managed_node_groups = {
+    al2 = {
+      ami_type = "AL2_x86_64"
+
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+    bottlerocket = {
+      ami_type = "BOTTLEROCKET_x86_64"
+
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+}
